2598a6d43ca18971b67b9ee91ab88d5236c7d83f
MayzaLutfiSetyaji <mayzalutfi656@gmail.com>
2025-08-28 20:37:02 +0700

    Add files via upload

---
+++ lutfi_project1/src/main/java/com/mycompany/lutfi_project1/Lutfi_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lutfi_project1;
+
+/**
+ *
+ * @author mayza
+ */
+public class Lutfi_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Mayza Lutfi Setyaji");
+        System.out.println("NPM: 2420506033");
+        System.out.println("Alamat: Kebumen, Jawa tengah");
+        System.out.println("No.Telepon: 08123456789");
+    }
+}
